29d3ef144ccb9440555bbbcdf385c5ef2a6324ef
Akkahshh <akkahshh24@gmail.com>
2024-03-21 23:54:32 +0530

    add 2nd solution

--- stack/20_valid_parentheses.py
+++ stack/20_valid_parentheses.py
@@ -3,18 +3,32 @@ class Solution:
         # Last bracket in, first bracket out
         # Use stacks
 
-        stack = []
-        bracket_pairs = {"{":"}", "(":")", "[":"]"}
+        # # Solution 1:
+        # stack = []
+        # bracket_pairs = {"{":"}", "(":")", "[":"]"}
 
-        for b in s:
-            # opening bracket, add it to the stack
-            if b in bracket_pairs.keys():
-                stack.append(b)
-            # closing bracket
-            else:
-                # if stack is empty
-                if not stack or bracket_pairs[stack[-1]] != b:
-                    return False
-                stack.pop()
+        # for b in s:
+        #     # opening bracket, add it to the stack
+        #     if b in bracket_pairs.keys():
+        #         stack.append(b)
+        #     # closing bracket
+        #     else:
+        #         # if stack is empty
+        #         if not stack or bracket_pairs[stack[-1]] != b:
+        #             return False
+        #         stack.pop()
 
-        return True if not stack else False
+        # return True if not stack else False
+    
+        # Solution 2:
+        while True:
+            old_len = len(s)
+
+            s = s.replace("{}", "").replace("[]", "").replace("()", "")
+
+            new_len = len(s)
+
+            if new_len == old_len:
+                break
+
+        return len(s) == 0
